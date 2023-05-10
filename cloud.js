2c69e7e2b3008466dd2ad5832580481983ae1823
Vadim Raitses <vadim.raitses@hubject.com>
2023-05-10 23:17:19 +0200

    using cloud provider

--- cloud.js
+++ cloud.js
@@ -5,6 +5,7 @@ import * as fs from 'fs';
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 const url = process.argv[2];
+const isHeadless = process.argv[3];
 if (!url) {
   throw "Please provide a URL ebay deals url";
 }
@@ -12,10 +13,10 @@ if (!url) {
 let data_folder = ''
 let set_log_folder = (category) => {
   data_folder = `data/${category}`;
-  //fs.mkdirSync(data_folder, { recursive: true });
 }
 
 let write_data_file = (data) => {
+  console.log(`${data_folder}`)
   fs.writeFileSync(`${data_folder}.json`, JSON.stringify(data), { encoding: "utf8" });
 }
 
@@ -30,7 +31,7 @@ async function run() {
       "--disable-accelerated-2d-canvas",
       "--disable-gpu"],
     ignoreHTTPSErrors: true,
-    headless: false,
+    headless: isHeadless,
   });
   const page = await browser.newPage();
   await page.goto(url);
@@ -76,9 +77,8 @@ async function run() {
           $('.load-more-btn').click();
         });
       }
-      console.log('getching products')
+      console.log('fetching products')
       const products = await page.evaluate(() => {
-        let categories = ["deals_of_the_week", "hot_new_arrivals", "hot_best_seller", "trends"];
         let map = [];
         $(".col").each(function (index, item) {
           try {
@@ -89,46 +89,29 @@ async function run() {
             if (data_array.length > 1) {
               obj.data_array = data_array;
 
-              obj._ids = $(item).children()[0].getAttribute("data-listing-id");
+              obj.item_id = $(item).children()[0].getAttribute("data-listing-id");
               obj.link = $(item).find("a")[0].href;
-              obj.image = $(item).find("img")[0].src;
-              obj.name = data_array[0].trim();
+              obj.image_url = $(item).find("img")[0].src;
+              obj.title = data_array[0].trim();
 
-              obj.last_signed_price = data_array[1];
-              obj.last_price = Number(data_array[1].replace("$", "").trim().replace(",", ""));
+              obj.price_signed = data_array[1];
+              obj.price = Number(data_array[1].replace("$", "").trim().replace(",", ""));
 
-              obj.discount = 0;
-              obj.pervious_price = "";
-              obj.pervious_signed_price = "";
-
-              if (data_array.length > 2) {
-                let pervious_price = data_array[2].match(/\$[\d\.\,]{1,9}/g);
-                if (pervious_price) {
-                  obj.pervious_price = Number(pervious_price[0].replace("$", "").trim().replace(",", ""));
-                  obj.pervious_signed_price = pervious_price[0];
-                }
-                let discount = data_array[2].match(/\d{1,3}%/g);
-                if (discount) { obj.discount = Number(discount[0].replace("%", "")); }
-              }
-              // obj.father_category=fatherNode;
-              // obj.child_category=childNode;
-              // obj.site_category = site_category;
-
+          
+     
               obj.vendor = "ebay";
               obj.weight = 1;
-              obj.ui_category_weight = categories[Math.round(Math.random() * 3)];
-              if (!obj.image.startsWith("https://ir.ebaystatic.com"))
+              if (!obj.image_url.startsWith("https://ir.ebaystatic.com"))
                 map.push(obj);
             }
           } catch (err) {
             console.log(err);
           }
         });
-        console.log(map);
         return map;
       });
       write_data_file(products)
-      console.log(products);
+      //console.log(products);
       await sleep(1000);
     }
   };
